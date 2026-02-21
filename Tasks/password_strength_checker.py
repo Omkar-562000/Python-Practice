bd593f21775079b2a7c3caf5af759d4ce0bfa377
Omkar <omkarghanekar56@gmail.com>
2026-02-22 00:57:16 +0530

    Task 2 begin

--- Tasks/password_strength_checker.py
+++ Tasks/password_strength_checker.py
@@ -1,2 +1,26 @@
-value = "False"
-print(bool(value))
+import getpass # This is the module 
+print("=" * 40)
+print("PASSWORD STRENGTH CHECKER".center(40))
+print("=" * 40)
+
+password = getpass.getpass("[?] Enter Password : ").strip()
+
+print()
+print(f"[*] Password Length     :       {len(password)}")
+print(f"[*] Password            :       {password}")
+
+if len(password) <= 8 :
+    print("Pass")
+else:
+    print("Fail")
+
+if any(char.isupper() for char in password) :
+    print("Pass")
+else:
+    print("Fail")
+
+if any(char.isdigit() for char in password) :
+    print("Pass")
+else:
+    print("Fail")
+
